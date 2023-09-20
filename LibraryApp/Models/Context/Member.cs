58a6fc30f132cb608348b1838a3ed803c1f72d5d
yusuf <yusuf@DESKTOP-EA2SJ5P>
2023-09-20 22:08:56 +0300

    Proje dosyası ekle.

---
+++ LibraryApp/Models/Context/Member.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using AracKiralamaOtomasyonu.Models.Context;
+
+namespace AracKiralamaOtomasyonu.Models
+{
+    public class Member
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string IDNumber { get; set; }
+        public string Address { get; set; }
+        public System.DateTime BirthDay { get; set; }
+        public bool Gender { get; set; }
+        public string EMail { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }   
+        public bool Verification { get; set; }
+        public System.DateTime CreateDay { get; set; }
+        public System.DateTime LastLoginTime { get; set; }
+        public virtual ICollection<Recipient> Recipient  { get; set; }
+
+    }
+}
---
+++ LibraryApp/Models/Maps/FilesMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using AracKiralamaOtomasyonu.Models.Context;
+
+namespace AracKiralamaOtomasyonu.Models.Map
+{
+    public class FilesMap : EntityTypeConfiguration<Files>
+    {
+        public FilesMap()
+        {
+            this.ToTable("Files");
+            this.Property(p => p.ID).HasColumnType("int");
+            this.Property(p => p.ID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            this.Property(p => p.Url).HasColumnType("nvarchar").HasMaxLength(100);
+            this.Property(p => p.Type).HasColumnType("nvarchar").HasMaxLength(30);
+        }
+
+    }
+}
